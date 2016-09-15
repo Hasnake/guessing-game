c1f693b503c77159a30d9e00ca0fc34ec0fae9ec
Hasnake <haileyesus46@gmail.com>
2016-09-15 03:03:56 -0700

    my first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+var userName = prompt('What\'s your name?');
+   console.log('Welcome to my site ' + userName);
+
+   var questionOne = prompt('Was I born in Washington?');
+   questionOne = questionOne.toLowerCase();
+
+   if (questionOne === 'no'  || questionOne === 'n') {
+     console.log('Correct, I was born in Ethiopia!')
+   } else if (questionOne === 'yes'  || questionOne === 'y') {
+     console.log('Not true, I was born in Ethiopia!')
+   } else {
+
+   } var questionTwo = prompt('Do you want to know me better');
+   questionTwo = questionTwo.toLowerCase();
+
+     if (questionTwo === 'yes'  || questionTwo === 'y') {
+       console.log('Great, please read on ' + userName);
+     } else if (questionTwo === 'no'  || questionTwo === 'n'){
+       console.log('it is shown there keep on reading')
+     }
+     var questionThree = prompt('Do you like my photo?');
+     questionThree = questionThree.toLowerCase();
+
+       if (questionThree === 'yes'  || questionThree === 'y'){
+       console.log('Thank you very much.Yo are so awesome too!');
+     } else if (questionThree === 'no'  || questionThree === 'n'){
+       console.log('oh boy better to like that!.');
+     }
+     var questionFour = prompt('Do you want to perform some music?');
+     questionFour = questionFour.toLowerCase();
+
+     if (questionFour === 'yes'  || questionFour === 'y') {
+       console.log('come visit me.');
+
+     } else if (questionFour === 'no'  || questionFour === 'n'){
+       console.log('Don\'t  ask such question ');
+     }
